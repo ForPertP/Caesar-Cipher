008cc49660f6ad265d67d3991ee682e444c7df92
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-12 00:48:18 +0900

    Update Caesar-Cipher.cpp

--- Caesar-Cipher.cpp
+++ Caesar-Cipher.cpp
@@ -16,3 +16,31 @@ string caesarCipher(string s, int k)
     return result;
 }
 
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string caesarCipher(string s, int k) {
+    k %= 26;
+    
+    for (char &c : s) {
+        if (isalpha(c)) {
+            char base = islower(c) ? 'a' : 'A';
+            c = (c - base + k) % 26 + base;
+        }
+    }
+    return s;
+}
+
+int main() {
+    int n, k;
+    string s;
+
+    cin >> n >> s >> k;
+    cout << caesarCipher(s, k) << endl;
+
+    return 0;
+}
