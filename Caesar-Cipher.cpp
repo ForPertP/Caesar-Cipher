c0fd880c2b5c98da16b3a986e8d3351510790275
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-13 23:47:31 +0900

    Update Caesar-Cipher.cpp

--- Caesar-Cipher.cpp
+++ Caesar-Cipher.cpp
@@ -1,23 +1,3 @@
-string caesarCipher(string s, int k)
-{
-    std::string result;
-    
-    for(auto &word: s)
-    {
-        if (isalpha(word))
-        {
-            char temp = isupper(word) ? 'A' : 'a';
-            word = (word - temp + k) % 26 + temp;
-        }
-        
-        result += word;
-    }
-    
-    return result;
-}
-
-
-
 #include <iostream>
 #include <string>
 
