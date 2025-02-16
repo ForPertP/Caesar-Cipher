4cc4b48d580ad166356ad1a265d21eddf849e915
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-17 05:10:16 +0900

    Update Caesar-Cipher.cs

--- Caesar-Cipher.cs
+++ Caesar-Cipher.cs
@@ -1,1 +1,30 @@
-Caesar-Cipher
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static string caesarCipher(string s, int k)
+    {
+    }
+}
