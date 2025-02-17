8fdc6afed2e37d57271963d880793592747cf0af
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-18 01:47:36 +0900

    Update Caesar-Cipher.cs

--- Caesar-Cipher.cs
+++ Caesar-Cipher.cs
@@ -28,3 +28,25 @@ class Result
     {
     }
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string s = Console.ReadLine();
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.caesarCipher(s, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
