5f6514dd28ade62f450b8ee89924ac7037639d3e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-21 03:21:23 +0900

    Update Caesar-Cipher.java

--- Caesar-Cipher.java
+++ Caesar-Cipher.java
@@ -23,4 +23,26 @@ class Result {
 
     public static String caesarCipher(String s, int k) {
     }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
